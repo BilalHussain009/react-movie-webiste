7d79afefad1fc2aa04db36c0246556df2bc6f70e
BilalHussain009 <bilalhussain.work@gmail.com>
2019-09-15 16:45:28 +0500

    Update

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,13 @@
 import * as firebase from 'firebase';
 
 const config = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+  apiKey: "AIzaSyApIXLxw-VBsWzxZyiD6rnuYqoJ2N9cTIY",
+    authDomain: "expensify-bilal-app.firebaseapp.com",
+    databaseURL: "https://expensify-bilal-app.firebaseio.com",
+    projectId: "expensify-bilal-app",
+    storageBucket: "expensify-bilal-app.appspot.com",
+    messagingSenderId: "627291763869",
+    appId: "1:627291763869:web:b40f40447c47e625"
 };
 
 firebase.initializeApp(config);
